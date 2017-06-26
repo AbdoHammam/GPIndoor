5d6784bd13125afaf07484f2af257e34bcfec3d9
AbdoHammam <abdelrahman.hammam95@gmail.com>
2017-06-26 16:33:03 +0200

    Test monitoring: “flip to sleep

--- app/src/main/java/com/example/abdo/locatemeindoor/LocateMeIndoor.java
+++ app/src/main/java/com/example/abdo/locatemeindoor/LocateMeIndoor.java
@@ -1,10 +1,17 @@
 package com.example.abdo.locatemeindoor;
 
 import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 
 import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
 import com.estimote.coresdk.service.BeaconManager;
+import com.estimote.mgmtsdk.feature.settings.api.Beacon;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -23,8 +30,39 @@ public class LocateMeIndoor extends Application {
                 beaconManager.startMonitoring(new BeaconRegion(
                         "monitored region",
                         UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"),
-                        22504, 48827));
+                        22504, 48827)); // TODO : check this UUID from part-2 Start monitoring
             }
         });
+        // Geofence
+        beaconManager.setMonitoringListener(new BeaconManager.BeaconMonitoringListener() {
+            @Override
+            public void onEnteredRegion(BeaconRegion beaconRegion, List<com.estimote.coresdk.recognition.packets.Beacon> list) {
+                    // TODO : Add notification ( call show notification fn below ) 
+            }
+
+            @Override
+            public void onExitedRegion(BeaconRegion beaconRegion) {
+
+            }
+        });
+
     }
+    public void showNotification(String title, String message) {
+        Intent notifyIntent = new Intent(this, MainActivity.class);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivities(this, 0,
+                new Intent[] { notifyIntent }, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+        notification.defaults |= Notification.DEFAULT_SOUND;
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
+    }
+
 }
